5854793616aa7afb00cdb7d427e11e34d2cd20a8
Olav Nikolai Risdal Breivik <olavbr@nr.no>
2023-11-30 14:29:06 +0100

    integration points, bugfix

--- R/pred.R
+++ R/pred.R
@@ -7,13 +7,15 @@
 constructIntPoints<-function(conf,confPred){
   utmCRS = CRS(paste0("+proj=utm +zone=", conf$zone," +datum=WGS84 +units=km +no_defs"))
   strata_utm <- spTransform(conf$strata,utmCRS)
-
   points = sp::makegrid(strata_utm,cellsize = confPred$cellsize)
   pointsSP = sp::SpatialPoints(points,utmCRS)
-  inside = rep(1,dim(pointsSP@coords)[1])
-  inside[which(is.na(over(pointsSP,strata_utm)))] = 0
+
+  pointsSPXY = suppressWarnings(spTransform(pointsSP,conf$strata@proj4string))
+  inside = rep(1,dim(pointsSPXY@coords)[1])
+  inside[which(is.na(over(pointsSPXY,conf$strata)))] = 0 #over() with lat-lon, some points between strata were not assigned to a strata when using over() with utm
   points = points[which(inside==1),]
   pointsSP = sp::SpatialPoints(points,utmCRS)
+  pointsSPXY =  suppressWarnings(spTransform(pointsSP,conf$strata@proj4string))
 
   #Define data frame with integration points to be returned
   locUTM = data.frame(points[,1],points[,2]) #To be returned
@@ -21,11 +23,9 @@ constructIntPoints<-function(conf,confPred){
 
   idxStrata = rep(-1,dim(locUTM)[1])
   for(i in 1:nrow(conf$strata)){
-    insideThis =  which(!is.na(over(pointsSP,strata_utm[i,])[,1]))
+    insideThis =  which(!is.na(over(pointsSPXY,conf$strata[i,])[,1]))
     idxStrata[insideThis] = i
   }
 
   return(list(locUTM = locUTM, idxStrata = idxStrata))
 }
-
-
