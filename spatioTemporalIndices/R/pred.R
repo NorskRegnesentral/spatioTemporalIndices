4dfbc41c0c293c4cbc24b64d1765005dfdf150b1
FBZimmermann <fabian.zimmermann@hi.no>
2024-02-26 14:36:08 +0100

    strata buffer

--- spatioTemporalIndices/R/pred.R
+++ spatioTemporalIndices/R/pred.R
@@ -10,13 +10,13 @@ constructIntPoints<-function(conf,confPred){
   points = st_make_grid(strata_utm,cellsize=c(confPred$cellsize,confPred$cellsize),what="centers")
   #points = st_sample(strata_utm,size = round(sum(as.numeric(st_area(strata_utm)))*(1/1.852)^2/confPred$cellsize,0),type="regular")
   points = st_as_sf(points)
-
+  
   #Define data frame with integration points to be returned
-  points = st_join(points,strata_utm,left=FALSE)
+  points = st_join(points,st_buffer(strata_utm,1),left=FALSE)
   
   locUTM = data.frame(st_coordinates(points)) #To be returned
   colnames(locUTM) = c("UTMX", "UTMY")
-
+  locUTM = locUTM[!duplicated(locUTM),]
   # idxStrata = rep(-1,dim(locUTM)[1])
   # for(i in 1:nrow(conf$strata)){
   #     insideThis =  which(!is.na(over(pointsSPXY,test[i,])[,1]))
