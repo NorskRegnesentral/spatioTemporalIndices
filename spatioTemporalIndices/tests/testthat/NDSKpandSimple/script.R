deff8b24edb0b93fc20d09979e6b6099c2160673
FBZimmermann <fabian.zimmermann@hi.no>
2025-02-04 12:29:08 +0100

    NDSKpandSimple test

--- spatioTemporalIndices/tests/testthat/NDSKpandSimple/script.R
+++ spatioTemporalIndices/tests/testthat/NDSKpandSimple/script.R
@@ -1,6 +1,6 @@
 suppressMessages(library(spatioTemporalIndices))
 
-dat_l = readRDS("NDSKpandLength/NDSKpand2018-2020_length.rds")
+dat_l = readRDS("NDSKpandSimple/NDSKpand2018-2020_length.rds")
 
 conf_l = defConf(years = 2020:2020,
                dLength = 1, # length intervall in mm
@@ -14,13 +14,13 @@ conf_l = defConf(years = 2020:2020,
                splineDepth = c(6,1),
                reduceLength = 3,
                rwBeta0 = 1,
-               stratasystem = list(dsn="NDSKpandLength/strata", layer = "shrimp_areas_NSSK"),
+               stratasystem = list(dsn="NDSKpandSimple/strata/", layer = "shrimp_areas_NSSK"),
                minDepth=50,maxDepth=600,
                trawlWidth=11.7,
                applyALK=0,
                strataReport=0)
 
-confPred = defConfPred(conf=conf_l,Depth="DATA",cellsize = 20)
+confPred = defConfPred(conf=conf_l,Depth="NDSKpandSimple/gebco_2023_NDSK.nc",cellsize = 20)
 
 # run model
 run = fitModel(dat_l,conf_l,confPred,ignore.parm.uncertainty = TRUE)
@@ -37,6 +37,6 @@ expect_equal(resultsOut$objectiveExp, resultsExp$objectiveExp,tolerance = 1e-4)
 
 if(FALSE){
   resultsExp = list(AIC = AIC(run,run2))
-  save(resultsExp,file = "NDSKpandSimple/resultsExp.RData")
+  save(resultsExp,file = "tests/testthat/NDSKpandSimple/resultsExp.RData")
 }
 
