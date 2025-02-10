287b096afb03860c6f1788a98d2ce8cb9d02caf8
Olav Nikolai Risdal Breivik <olavbr@nr.no>
2025-02-10 07:48:59 +0100

    posterior mean

---
+++ spatioTemporalIndices/R/posteriorMean.R
@@ -0,0 +1,46 @@
+##' Posterior mean
+##' @param run The fitted model
+##' @details Return posterior mean of the index (using the bias-correction flag in TMB::sdreport for the catch-at-length random effects only)
+##' @export
+posteriorMean = function(run){
+
+  if(run$conf_l$applyALK ==0){
+    #Do bias-correct one year at the time to reduce memory consuption
+    split = apply(run$obj$env$ADreportIndex()$logLengthIndex,1,function(f) list(f))
+    split = lapply(split,function(f)f[[1]])
+
+    sdrep <- TMB::sdreport(run$obj,bias.correct = TRUE,
+                           bias.correct.control = list(split = split,sd = FALSE),
+                           skip.delta.method = TRUE,
+                           getReportCovariance = FALSE)
+
+  }else{
+    #Remove random effect in ALK to reduce memory consumption
+    pl = run$pl
+    map = run$map
+
+    if(run$conf_alk$rwBeta0==1){
+      map$beta0_alk = as.factor(pl$beta0_alk*NA)
+    }
+    map$xS_alk = as.factor(pl$xS_alk*NA)
+    map$xST_alk = as.factor(pl$xST_alk*NA)
+
+    dat_l = run$dat_l
+    dat_alk = run$dat_alk
+    conf_l = run$conf_l
+    conf_alk = run$conf_alk
+    confPred = run$confPred
+    runMAPalk = fitModel(dat_l,conf_l, confPred,dat_alk,conf_alk,mapSet = map,parSet = pl,runModel = FALSE)
+
+    #Do bias-correct one year at the time to reduce memory consuption
+    split = apply(run$obj$env$ADreportIndex()$logAgeIndex,1,function(f) list(f))
+    split = lapply(split,function(f)f[[1]])
+
+    sdrep <- TMB::sdreport(runMAPalk$obj,bias.correct = TRUE,
+                           bias.correct.control = list(split = split,sd = FALSE),
+                           skip.delta.method = TRUE,
+                           getReportCovariance = FALSE, ,ignore.parm.uncertainty = TRUE)
+  }
+
+  return(sdrep)
+}
