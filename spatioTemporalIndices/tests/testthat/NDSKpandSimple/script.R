e094f91b9491f8930f49485ed1c1315aed0113e1
Olav Nikolai Risdal Breivik <olavbr@nr.no>
2025-02-18 07:11:25 +0100

    tests length simple

--- spatioTemporalIndices/tests/testthat/NDSKpandSimple/script.R
+++ spatioTemporalIndices/tests/testthat/NDSKpandSimple/script.R
@@ -4,22 +4,22 @@ dat_l = readRDS("NDSKpandSimple/NDSKpand2018-2020_length.rds")
 dat_l$station <- paste(format(dat_l$startdatetime,"%Y"),dat_l$station, sep = "_")
 
 conf_l = defConf(years = 2020:2020,
-               dLength = 1, # length intervall in mm
-               maxLength = 25, # Numeric = use directly; NULL = use input data to determine
-               minLength = 7, # Numeric = use directly; NULL = use input data to determine
-               cutoff = 20,
-               spatioTemporal = 0,
-               nugget = 1,
-               spatial = 0,
-               sunAlt=c(1,1),
-               splineDepth = c(6,1),
-               reduceLength = 3,
-               rwBeta0 = 0,
-               stratasystem = list(dsn="NDSKpandSimple/strata/", layer = "shrimp_areas_NSSK"),
-               minDepth=150,maxDepth=300,
-               trawlWidth=11.7,
-               applyALK=0,
-               strataReport=0)
+                 dLength = 1, # length intervall in mm
+                 maxLength = 25, # Numeric = use directly; NULL = use input data to determine
+                 minLength = 7, # Numeric = use directly; NULL = use input data to determine
+                 cutoff = 20,
+                 spatioTemporal = 0,
+                 nugget = 1,
+                 spatial = 0,
+                 sunAlt=c(1,1),
+                 splineDepth = c(6,1),
+                 reduceLength = 3,
+                 rwBeta0 = 1,
+                 stratasystem = list(dsn="NDSKpandSimple/strata/", layer = "shrimp_areas_NSSK"),
+                 minDepth=50,maxDepth=600,
+                 trawlWidth=11.7,
+                 applyALK=0,
+                 strataReport=0)
 
 confPred = defConfPred(conf=conf_l,Depth="NDSKpandSimple/gebco_2023_NDSK.nc",cellsize = 50)
 
@@ -27,14 +27,12 @@ confPred = defConfPred(conf=conf_l,Depth="NDSKpandSimple/gebco_2023_NDSK.nc",cel
 runCovariates = fitModel(dat_l,conf_l,confPred,ignore.parm.uncertainty = TRUE,silent = TRUE)
 
 ###Length dependent covariates:
-conf_l$years = 2019:2020
-confPred = defConfPred(conf=conf_l,cellsize = 50)
 conf_l$sunAlt=c(1,2)
 conf_l$splineDepth = c(6,2)
+confPred = defConfPred(conf=conf_l,Depth="NDSKpandSimple/gebco_2023_NDSK.nc",cellsize = 100)
 runLenghtDepCov = fitModel(dat_l,conf_l,confPred,ignore.parm.uncertainty = TRUE,silent = TRUE)
 
 #no Nugget
-conf_l$years = 2020
 conf_l$sunAlt=c(1,0)
 conf_lsplineDepth = c(6,0)
 conf_l$nugget = 0
@@ -61,7 +59,7 @@ conf_l = defConf(years = 2018:2020,
                  stratasystem = list(dsn="NDSKpandSimple/strata/", layer = "shrimp_areas_NSSK"),
                  trawlWidth=11.7,
                  applyALK=0)
-runNoReducedSpace = fitModel(dat_l,conf_l,confPred,ignore.parm.uncertainty = TRUE,silent = TRUE)
+runNoReducedSpace = fitModel(dat_l,conf_l,confPred,ignore.parm.uncertainty = TRUE,silent = FALSE)
 
 
 resultsOut = list(AIC = AIC(runCovariates,runNoNugget,runLenghtDepCov,runMissingDepth,runNoReducedSpace))
@@ -74,11 +72,11 @@ resultsOut$rlIndexNoReducedSpace = runNoReducedSpace$rl$logLengthIndex
 
 load("NDSKpandSimple/resultsExp.RData")
 expect_equal(resultsOut$AIC, resultsExp$AIC,tolerance = 1e-4)
-expect_equal(resultsOut$rlIndexCovariates, resultsExp$rlIndexCovariates,tolerance = 1e-3)
-expect_equal(resultsOut$rlIndexLenghtDepCov, resultsExp$rlIndexLenghtDepCov,tolerance = 1e-3)
-expect_equal(resultsOut$rlIndexNoNugget, resultsExp$rlIndexNoNugget,tolerance = 1e-3)
-expect_equal(resultsOut$rlIndexMissingDepth, resultsExp$rlIndexMissingDepth,tolerance = 1e-3)
-expect_equal(resultsOut$rlIndexNoReducedSpace, resultsExp$rlIndexNoReducedSpace,tolerance = 1e-3)
+expect_equal(resultsOut$rlIndexCovariates, resultsExp$rlIndexCovariates,tolerance = 1e-2)
+expect_equal(resultsOut$rlIndexLenghtDepCov, resultsExp$rlIndexLenghtDepCov,tolerance = 1e-2)
+expect_equal(resultsOut$rlIndexNoNugget, resultsExp$rlIndexNoNugget,tolerance = 1e-2)
+expect_equal(resultsOut$rlIndexMissingDepth, resultsExp$rlIndexMissingDepth,tolerance = 1e-2)
+expect_equal(resultsOut$rlIndexNoReducedSpace, resultsExp$rlIndexNoReducedSpace,tolerance = 1e-2)
 
 test_that("Plot runs without error", {
   expect_silent(plotResults(runLenghtDepCov, what = "sunAlt"))
@@ -95,4 +93,3 @@ if(FALSE){
   resultsExp$rlIndexNoReducedSpace = runNoReducedSpace$rl$logLengthIndex
   save(resultsExp,file = "NDSKpandSimple/resultsExp.RData")
 }
-
